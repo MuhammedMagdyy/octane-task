5a98a94903718db3ad294c75d3c1b3679b148ad3
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-06-23 22:13:02 +0300

    feat: refactor application structure and add PostgreSQL conifgs with shared base entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          database: config.get<string>('POSTGRES_DB'),
+          username: config.get<string>('POSTGRES_USER'),
+          password: config.get<string>('POSTGRES_PASSWORD'),
+          port: config.get<number>('POSTGRES_PORT'),
+          host: config.get<string>('POSTGRES_HOST'),
+          synchronize: config.get<string>('NODE_ENV') !== 'production',
+          entities: [],
+        };
+      },
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/common/entities/base.entity.ts
@@ -0,0 +1,34 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { CURRENT_TIMESTAMP } from '../utils/constants';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: number;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => CURRENT_TIMESTAMP,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => CURRENT_TIMESTAMP,
+    onUpdate: CURRENT_TIMESTAMP,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deletedAt: Date;
+}
