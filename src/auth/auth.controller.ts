72b62684e539d6aee4a65b885e5c516c7c94bce1
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-06-26 20:41:26 +0300

    fix: change HTTP status code for login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,12 +8,12 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  @HttpCode(HttpStatus.CREATED)
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
