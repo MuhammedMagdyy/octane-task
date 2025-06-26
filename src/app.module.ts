34e13ae647b7830c813f85a1a7ba8ee589c54463
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-06-26 20:46:49 +0300

    feat: add Logger Middleware for HTTP request logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
@@ -7,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 import { Book } from './books/entities/book.entity';
 import { CommonModule } from './common/common.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { ReadingInterval } from './reading-intervals/entities/reading-intervals.entity';
 import { ReadingIntervalsModule } from './reading-intervals/reading-intervals.module';
 import { RecommendationsModule } from './recommendations/recommendations.module';
@@ -48,4 +54,10 @@ import { UsersModule } from './users/users.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '/*path', method: RequestMethod.ALL });
+  }
+}
